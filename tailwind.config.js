4820330677f79b88cac960c27caa6e513165f004
mBadar-dev <m.badar.dev@gmail.com>
2024-12-28 12:47:48 +0500

    add fonts from googel font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./main/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily:
+    {
+      'poppins': ['Poppins', 'sans-serif'],
+      'montserrat': ['Montserrat', 'sans-serif'],
+      'Noto':['Noto Sans JP', 'serif']
+    },
   },
   plugins: [],
 }
